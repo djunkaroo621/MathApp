9cb17aabe6a86dd2d80b33cacf4d32c25fa82f55
davejang <djunkaroo621@users.noreply.github.com>
2023-11-09 07:38:23 -0500

    Create style.css

---
+++ style.css
@@ -0,0 +1,63 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    max-width: 400px;
+    margin: 0 auto;
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    text-align: center;
+    font-size: 24px;
+    color: #4CAF50;
+}
+
+.question-box {
+    text-align: center;
+    margin-bottom: 20px;
+    position: relative;
+}
+
+.question-box img {
+    max-width: 100px;
+    position: absolute;
+    top: -20px;
+    left: calc(50% - 50px);
+}
+
+input[type="number"] {
+    width: 100%;
+    padding: 10px;
+    margin-top: 10px;
+    font-size: 18px;
+    text-align: center;
+    border: 2px solid #4CAF50;
+    border-radius: 5px;
+}
+
+button {
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin-top: 10px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+p#feedback {
+    text-align: center;
+    font-weight: bold;
+    margin-top: 10px;
+    font-size: 18px;
+    color: #4CAF50;
+}
